8d5d1506e0ed51bc100f022d2aae351f281dd6eb
Omar <omarta@code.edu.az>
2024-01-27 20:39:11 +0400

    ...

---
+++ assets/js/script.js
@@ -0,0 +1,67 @@
+"use scrict";
+
+// var surname = "Surname"
+// console.log(surname);
+// let address = "address"
+// console.log(address);
+
+// const age = 16;
+// console.log(age);
+
+// const name = 'test'
+// console.log(name);
+
+
+
+// const stu = {
+//     name: "Name",
+//     surname: 'Surname',
+//     age: 16
+// };
+
+// console.log(stu.name + ' --- ' + stu.surname );
+
+// const stu2 = {
+//     name: 'name2',
+//     surname: 'surname2',
+//     age: 17
+// }
+
+
+// stu.name = 'annd'
+// console.log(stu.name + ' --- ' + stu.surname );
+
+
+// let name = 'XXX';
+// let surname = 'YYY';
+
+// // let fullName = name + " " + surname
+// let fullName = `${name} ${surname}`;
+
+// console.log(fullName);
+
+
+// let ages = [22,44,55,66,77,88];
+
+// console.log(ages[ages.length-1]);
+
+
+
+
+// let num1 = 5;
+// let num2 = 10;
+
+// console.log(num1 ** num2);
+// console.log(num2%num1);
+// console.log(num2 / num1);
+
+
+// let num3 = "55";
+// let num4 = 55;
+
+// if(num4 === num3){
+//     console.log('yes');
+// }
+// else{
+//     console.log('no');
+// }
